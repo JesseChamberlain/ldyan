9e7a2b6cf49c6e17038877f68737a42daa29df16
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-07-30 11:03:42 -0400

    Setup API endpoint for Blocks

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :songs, only: [:index, :show]do
+      resources :songs, only: [:index, :show] do
         resources :blocks, only: [:index]
       end
     end
--- spec/factories/block_factory.rb
+++ spec/factories/block_factory.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :block do
     sequence(:name) {|n| "A#{n}"}
-    name "I"
-    color "green"
     repetitions "4"
     measures "8"
     time_signature_over "6"
     time_signature_under "8"
     musical_key "C Arabic"
+    song_id "4"
+    color "green"
   end
 end
