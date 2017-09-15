5e19a35baf469cce53eb19cae4e5b6cb4deeb12b
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-09-15 13:02:42 -0400

    About page rendering successfully

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import LandingPageContainer from './containers/LandingPageContainer';
+import AboutPageContainer from './containers/AboutPageContainer';
 import SongsIndexContainer from './containers/SongsIndexContainer';
 import SongShowContainer from './containers/SongShowContainer';
 
@@ -9,7 +10,7 @@ const App = (props) => {
     <Router>
       <div>
         <Route exact path="/" render={()=><LandingPageContainer userId={props.userId}/>}/>
-        <Route exact path="/about" component={SongsIndexContainer}/>
+        <Route exact path="/about" component={AboutPageContainer}/>
         <Route exact path="/songs" component={SongsIndexContainer}/>
         <Route exact path="/songs/:id" component={SongShowContainer}/>
       </div>
